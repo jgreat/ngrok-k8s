ea6cd274a5c7da43129057c5afcbf77d36b9f543
Jason Greathouse <jgreat@jgreat.me>
2020-02-27 11:32:06 -0600

    some more startup logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Starting entrypoint.sh"
+
 NGROK_HOME="/home/ngrok/.ngrok2"
 NGROK_CONFIG="${NGROK_HOME}/ngrok.yml"
 NGROK_IMPORT_CONFIG="/maps/ngrok/ngrok.yml"
@@ -11,12 +13,16 @@ if [ ! -d "${NGROK_HOME}" ]; then
 fi
 
 if [ -f ${NGROK_IMPORT_CONFIG} ]; then
+    echo "Importing Config from ConfigMap"
     cp ${NGROK_IMPORT_CONFIG} ${NGROK_CONFIG}
     echo "web_addr: 0.0.0.0:4040" >> ${NGROK_CONFIG}
 fi
 
 if [ ! -z "${NGROK_AUTHTOKEN}" ]; then
+    echo "Adding AUTHTOKEN to ${NGROK_CONFIG}"
     echo "authtoken: ${NGROK_AUTHTOKEN}" >> ${NGROK_CONFIG}
 fi
+
+echo "Finished entrypoint.sh"
 
 exec "$@"
